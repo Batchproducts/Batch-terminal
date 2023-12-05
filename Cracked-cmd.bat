e2be7c3353081842e92bed112a52d7402c014de8
aswdBatch <152989518+aswdBatch@users.noreply.github.com>
2023-12-05 16:43:56 +0200

    Update Cracked-cmd.bat

--- Cracked-cmd.bat
+++ Cracked-cmd.bat
@@ -1,5 +1,12 @@
 @echo off
+cls
+echo © Batchproducts 2023
+echo CRACKED TERMINAL
 :loop
-set /p command=">"
+set /p command="?:\CRACKED.CMD>"
 %command%
+goto loop
+
+:er
+echo LOL YOU FOUND ME
 goto loop
